53bd13f03cfffb58e1d079540161b4980d295391
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 11:44:03 +0100

    Update ativ6.php

--- ativ6.php
+++ ativ6.php
@@ -3,11 +3,30 @@ $a=23;
 $b=45;
 $soma=$a+$b;
 echo "23+45=".$soma;
-?>
 
-<?php
+
 $a=65;
 $b=5;
-$c10
-$media=$a+$b+$c/3;
+$c=10;
+$media=($a+$b+$c)/3;
+echo "<br><br>65+5+10/3=".$media;
+
+
+$x=41;
+$y=6;
+$quoci=$x/$y;
+$resto = ($x % $y);
+echo "<br><br>41/6=".$quoci;
+echo "<br>resto=".$resto;
+
+echo "<br><br>O João foi à oficina e a
+reparação do carro é de 78,00 € sem IVA.";
+$preco=78;
+$iva=0.23;
+$soma=$preco*$iva;
+$quoci=$preco+$iva+$soma;
+echo "<br> O João com iva paragara ".$quoci;
+
+
+
 ?>
